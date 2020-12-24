b29055792de33c8fd307f44c09942a665ddc5af0
smozhevsky <smozhevsky@gmail.com>
2020-12-24 15:17:26 +0300

    Changed the structure of the application
    Delete real-time clock
    
    Fixed checking for an empty string

--- js/script.js
+++ js/script.js
@@ -9,35 +9,12 @@ let itemsArray = localStorage.getItem("items")
 localStorage.setItem("items", JSON.stringify(itemsArray));
 const data = JSON.parse(localStorage.getItem("items"));
 
-/* (если число меньше десяти, перед числом добавляем ноль) */
-function zero_first_format(val) {
-  if (val < 10) val = "0" + val;
-  return val;
-}
-
-/* функция получения текущей даты и времени */
-function realTime() {
-  var current_datetime = new Date();
-  var day = zero_first_format(current_datetime.getDate());
-  var month = zero_first_format(current_datetime.getMonth() + 1);
-  var year = current_datetime.getFullYear();
-  var hours = zero_first_format(current_datetime.getHours());
-  var minutes = zero_first_format(current_datetime.getMinutes());
-  var seconds = zero_first_format(current_datetime.getSeconds());
-
-  return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
-}
-
-// Обновление каждую сек.
-setInterval(function () {
-  document.querySelector(".current-date").innerHTML = realTime();
-}, 0);
-
 //Добавление таски в список
 const taskAdd = (task) => {
-  // if (formText.value.length < 1) {
-  //   return;
-  // }; Почему-то из-за проверки не фиксится ol при перезагрузке
+  if (task.length < 1) {
+    return;
+  };
+
   const li = document.createElement("li");
   li.textContent = task;
   li.setAttribute("class", "output-item");
@@ -46,15 +23,17 @@ const taskAdd = (task) => {
 
 // Добавление таски по клику на btn
 formButtonAdd.addEventListener("click", () => {
-  itemsArray.push(formText.value);
-  localStorage.setItem("items", JSON.stringify(itemsArray));
+  if (formText.value) {
+    itemsArray.push(formText.value);
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+  }
   taskAdd(formText.value);
   formText.value = ""; //Чтобы не удалять таску из поля ввода каждый раз
 });
 
 // Добавление таски по нажатию enter
 formText.addEventListener("keypress", function (e) {
-  if (13 == e.keyCode) {
+  if (13 == e.keyCode && formText.value) {
     itemsArray.push(formText.value);
     localStorage.setItem("items", JSON.stringify(itemsArray));
     taskAdd(formText.value);
@@ -62,11 +41,12 @@ formText.addEventListener("keypress", function (e) {
   }
 });
 
+// Показываем, что есть в Local storage
 data.forEach(item => {
   taskAdd(item);
 });
 
-// Del button
+// Удаление списка и очистка Local storage
 formButtonDel.addEventListener("click", function () {
   localStorage.clear();
   while (ol.firstChild) {
